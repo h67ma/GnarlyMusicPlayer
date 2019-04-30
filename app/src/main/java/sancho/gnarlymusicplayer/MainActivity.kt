595ecef619125a7ad708e1a0010a0e3a2d80317c
Sancho <szycikm@gmail.com>
2019-04-30 21:26:16 +0200

    put all layouts inside one layout file

--- app/src/main/java/sancho/gnarlymusicplayer/MainActivity.kt
+++ app/src/main/java/sancho/gnarlymusicplayer/MainActivity.kt
@@ -9,8 +9,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
-import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
 import java.util.*
 
