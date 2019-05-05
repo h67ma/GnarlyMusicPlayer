51ea32cc560340aec767eb9161f9baae44989ce5
Sancho <szycikm@gmail.com>
2019-05-05 16:20:16 +0200

    migrated to androidX

--- app/src/main/java/sancho/gnarlymusicplayer/BookmarksAdapter.kt
+++ app/src/main/java/sancho/gnarlymusicplayer/BookmarksAdapter.kt
@@ -1,8 +1,8 @@
 package sancho.gnarlymusicplayer
 
 import android.content.Context
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.helper.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
--- app/src/main/java/sancho/gnarlymusicplayer/StartDragListener.kt
+++ app/src/main/java/sancho/gnarlymusicplayer/StartDragListener.kt
@@ -1,6 +1,6 @@
 package sancho.gnarlymusicplayer
 
-import android.support.v7.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 
 interface StartDragListener
 {
